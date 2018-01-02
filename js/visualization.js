2adf3557a806655c4a81c32a895a64d577e0f577
Andreia Machado <andreiarfmachado4@gmail.com>
2018-01-02 11:13:27 +0000

    ident solutiongit add .

--- js/visualization.js
+++ js/visualization.js
@@ -1,120 +1,120 @@
 function draw(data) {
-	var dates = [];
-	var years = [];
-	var occurencesPerYear = [];
-	var newDataSet = [];
+    var dates = [];
+    var years = [];
+    var occurencesPerYear = [];
+    var newDataSet = [];
 
-	var margin = 40;
-	var width = 800;
-	var height = 600;
+    var margin = 40;
+    var width = 800;
+    var height = 600;
 
-	data.forEach(function(d) {
-		dates.push(d.datetime);
-	});
+    data.forEach(function(d) {
+        dates.push(d.datetime);
+    });
 
-	dates.forEach(function(entry) {
-		var year = entry.split("/")[2].split(" ")[0];
+    dates.forEach(function(entry) {
+        var year = entry.split("/")[2].split(" ")[0];
 
-		if(years.indexOf(year) == -1)
-			years.push(year);
+        if(years.indexOf(year) == -1)
+            years.push(year);
 
-		if(occurencesPerYear[year] != null)
-			occurencesPerYear[year] += 1;
-		else
-			occurencesPerYear[year] = 1;
-	});
+        if(occurencesPerYear[year] != null)
+            occurencesPerYear[year] += 1;
+        else
+            occurencesPerYear[year] = 1;
+    });
 
-	//last year is an outlier
-	years.sort().pop();
+    //last year is an outlier
+    years.sort().pop();
 
-	years.sort().forEach(function(year) {
-		var obj = new Object;
-		obj["year"] = year;
-		obj["Sightings"] = occurencesPerYear[year];
-		newDataSet.push(obj);
-	});
+    years.sort().forEach(function(year) {
+        var obj = new Object;
+        obj["year"] = year;
+        obj["Sightings"] = occurencesPerYear[year];
+        newDataSet.push(obj);
+    });
 
-	var maxSightings = 0;
-	newDataSet.forEach(function(data){
-		if(data.Sightings > maxSightings)
-		maxSightings = data.Sightings;
-	});
+    var maxSightings = 0;
+    newDataSet.forEach(function(data){
+        if(data.Sightings > maxSightings)
+        maxSightings = data.Sightings;
+    });
 
-	var y_extent = [0, maxSightings];
-	var y_scale = d3.scaleLinear().range([height - margin, margin]).domain(y_extent).nice();
-	var x_extent = [newDataSet[0].year, newDataSet[newDataSet.length-1].year];
-	var x_scale = d3.scaleLinear().range([margin, width - margin]).domain(x_extent).nice();
-	var x_axis = d3.axisBottom(x_scale).tickFormat(d3.format("d"));
-	var y_axis = d3.axisLeft(y_scale).ticks(maxSightings/500);
+    var y_extent = [0, maxSightings];
+    var y_scale = d3.scaleLinear().range([height - margin, margin]).domain(y_extent).nice();
+    var x_extent = [newDataSet[0].year, newDataSet[newDataSet.length-1].year];
+    var x_scale = d3.scaleLinear().range([margin, width - margin]).domain(x_extent).nice();
+    var x_axis = d3.axisBottom(x_scale).tickFormat(d3.format("d"));
+    var y_axis = d3.axisLeft(y_scale).ticks(maxSightings/500);
 
-	var svg = d3.select("body")
-		.append("svg")
-		.attr("width", width)
-		.attr("height", height)
-		.style("background", "white");
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .style("background", "white");
 
-	var div = d3.select("body").append("div")
-		.attr("class", "tooltip")
-		.style("opacity", 0);
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
 
-	d3.select("svg").append("g").attr("class", "x axis").attr("transform", "translate(0," + (height - margin) + ")").call(x_axis);
-	d3.select("svg").append("g").attr("class", "y axis").attr("transform", "translate(" +  margin + ",0)").call(y_axis);
+    d3.select("svg").append("g").attr("class", "x axis").attr("transform", "translate(0," + (height - margin) + ")").call(x_axis);
+    d3.select("svg").append("g").attr("class", "y axis").attr("transform", "translate(" +  margin + ",0)").call(y_axis);
 
-	var circles = svg.selectAll("circle")
-		.data(newDataSet)
-		.enter()
-		.append("circle");
+    var circles = svg.selectAll("circle")
+        .data(newDataSet)
+        .enter()
+        .append("circle");
 
-	circles.attr("cx", function (d) {  return x_scale(d.year);  })
-		.attr("class", "dot")
-		.attr("cy", function(d){return y_scale(d.Sightings)})
-		.attr("r", function () { return 5; })
-		.on("mouseover", function(d) {
-			div.transition()
-				.style("opacity", .9)
-				.style("left", (d3.event.pageX-50) + "px")
-				.style("top", (d3.event.pageY-50) + "px")
-				.text("Year: " + d.year + "\n" + "Sightings: " + d.Sightings);
-		})
-		.on("mouseout", function(d) {
-			div.transition()
-				.style("opacity", 0);
-		});
+    circles.attr("cx", function (d) {  return x_scale(d.year);  })
+        .attr("class", "dot")
+        .attr("cy", function(d){return y_scale(d.Sightings)})
+        .attr("r", function () { return 5; })
+        .on("mouseover", function(d) {
+            div.transition()
+                .style("opacity", .9)
+                .style("left", (d3.event.pageX-50) + "px")
+                .style("top", (d3.event.pageY-50) + "px")
+                .text("Year: " + d.year + "\n" + "Sightings: " + d.Sightings);
+        })
+        .on("mouseout", function(d) {
+            div.transition()
+                .style("opacity", 0);
+        });
 
-	var line = d3.line()
-		.x(function (d) { return x_scale(d.year) })
-		.y(function (d) { return y_scale(d.Sightings) });
+    var line = d3.line()
+        .x(function (d) { return x_scale(d.year) })
+        .y(function (d) { return y_scale(d.Sightings) });
 
-	d3.select("svg").append("path").attr("d", line(newDataSet));
+    d3.select("svg").append("path").attr("d", line(newDataSet));
 
-	svg.append("text")
-		.attr("x", (width/2))
-		.attr("y", (margin/1.5))
-		.attr("text-anchor", "middle")
-		.attr("font-size", "30px")
-		.text("UFO Sightings over the years");
-		
-	svg.append("text")
-		.attr("text-anchor", "middle")
-		.attr("transform", "translate("+ (margin+18) +","+(height/4)+")rotate(270)")
-		.text("Ammount of Sightings");
-			
-	svg.append("text")
-		.attr("text-anchor", "middle")
-		.attr("transform", "translate("+ (width/1.1) +","+(height-(margin+6))+")")
-		.text("Years");
+    svg.append("text")
+        .attr("x", (width/2))
+        .attr("y", (margin/1.5))
+        .attr("text-anchor", "middle")
+        .attr("font-size", "30px")
+        .text("UFO Sightings over the years");
+        
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("transform", "translate("+ (margin+18) +","+(height/4)+")rotate(270)")
+        .text("Ammount of Sightings");
+            
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("transform", "translate("+ (width/1.1) +","+(height-(margin+6))+")")
+        .text("Years");
 
-	// Add reference text
-	svg.append("text")
-		.attr("x", 300)
-		.attr("y", 400)
-		.text("The X-Files TV-Show Release");
+    // Add reference text
+    svg.append("text")
+        .attr("x", 300)
+        .attr("y", 400)
+        .text("The X-Files TV-Show Release");
 
-	// Add reference line
-	svg.append("line")
-		.attr("x1", 410)
-		.attr("y1", 410)
-		.attr("x2", 592)
-		.attr("y2", 530)
-		.attr("class", "reference");
+    // Add reference line
+    svg.append("line")
+        .attr("x1", 410)
+        .attr("y1", 410)
+        .attr("x2", 592)
+        .attr("y2", 530)
+        .attr("class", "reference");
 }
--- js/visualization2.js
+++ js/visualization2.js
@@ -1,16 +1,16 @@
 function map(data) {
-	
-	var margin = 40;
+    
+    var margin = 40;
     var width = 1100;
     var height = 820;
 
     newDataSet = [];
 
     var svg = d3.select("body")
-    	.append("svg")
-    	.attr("width", width)
-    	.attr("height", height)
-    	.style("background", "lightblue");
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .style("background", "lightblue");
 
     data.forEach(function(d) {
         var obj = new Object;
@@ -22,28 +22,28 @@ function map(data) {
     });
 
     d3.json("data/countries.geo.json", function(data) {
-    	var group = svg.selectAll("g")
-    		.data(data.features)
-    		.enter()
-    		.append("g")
+        var group = svg.selectAll("g")
+            .data(data.features)
+            .enter()
+            .append("g")
 
         var projection = d3.geoMercator().scale(180);
         var path = d3.geoPath().projection(projection);
 
         var areas = group.append("path")
-    		.attr("d", path)
-    		.attr("class", "area")
-    		.attr("fill", "sienna");
+            .attr("d", path)
+            .attr("class", "area")
+            .attr("fill", "sienna");
 
         svg.selectAll(".mark")
-    		.data(newDataSet)
-        	.enter()
-        	.append("image")
-        	.attr('class','mark')
-        	.attr('width', 20)
-        	.attr('height', 20)
-        	.attr("xlink:href", "images/Alien.png")
-        	.attr("transform", function(d) { return "translate(" + projection([d.longitude,d.latitude]) + ")";});
+            .data(newDataSet)
+            .enter()
+            .append("image")
+            .attr('class','mark')
+            .attr('width', 20)
+            .attr('height', 20)
+            .attr("xlink:href", "images/Alien.png")
+            .attr("transform", function(d) { return "translate(" + projection([d.longitude,d.latitude]) + ")";});
     });
 
 }
